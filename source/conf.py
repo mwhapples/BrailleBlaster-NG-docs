64c3c66301989ead2f00301499eb65686200eb77
Michael Whapples <software@mdw.onl>
2025-04-11 17:05:10 +0100

    Some customisation for BrailleBlaster-NG

--- source/conf.py
+++ source/conf.py
@@ -6,15 +6,15 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'BrailleBlaster User Documentation'
+project = 'BrailleBlaster-NG User Documentation'
 copyright = '2025, American Printing House for the Blind'
 author = 'American Printing House for the Blind'
-release = '3.0'
+release = '2025.1.3'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = ["myst_parser", "sphinx_rtd_theme"]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +24,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
